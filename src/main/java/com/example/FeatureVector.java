1ab2f6e8b973a460d80e23bc6eade0e2d41d0dfa
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2024-04-06 21:05:22 +0200

    Cleanup

--- src/main/java/com/example/FeatureVector.java
+++ src/main/java/com/example/FeatureVector.java
@@ -1,12 +1,9 @@
 package com.example;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
